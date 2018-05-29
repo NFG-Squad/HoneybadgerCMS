ffe51194b867f27236542107358ed2ad9a1ce992
sseltzer <sseltzer431@gmail.com>
2018-05-29 14:48:40 -0400

    Added an error message if I write a route and forget to add a register function.

--- core/routes/RouteLoader.js
+++ core/routes/RouteLoader.js
@@ -6,7 +6,9 @@ class RouteLoader {
   static load(server) {
     routes.forEach(r => {
       const route = require(path.join(__dirname, '../../', r));
-      server.app.use('/', new route(server).register(server.router));
+      let routeInstance = new route(server);
+      if (!routeInstance['register']) throw new Error(`Your route class does not have a register function: ${r}`);
+      server.app.use('/', routeInstance.register(server.router));
     });
   }
 }
